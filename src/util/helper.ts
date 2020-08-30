bb62a38fbe5f03e3ca5472f609c938ebb5aa4c71
Ayush Mishra <ayushm017@gmail.com>
2020-08-30 14:30:18 +0530

    feat: added write file function

--- src/util/helper.ts
+++ src/util/helper.ts
@@ -1,4 +1,8 @@
+import fs from "fs";
+import path from "path";
+import prettier from "prettier";
 import vscode from "vscode";
+
 import Dependency from "../model/Dependency";
 
 export async function packageJsonExists(): Promise<boolean> {
@@ -13,22 +17,28 @@ export async function packageJsonExists(): Promise<boolean> {
 }
 
 // TODO: write to package.json
-export async function writeFile(path: string, contents: string) {}
+export async function writeJsonFile(file: string, contents: string): Promise<void> {
+	const encoded = new TextEncoder().encode(contents);
+	const buffer = Buffer.from(contents);
+
+	const filePath = path.join(vscode.workspace.rootPath || "", file);
+	fs.writeFileSync(filePath, prettier.format(contents, { parser: "json" }), "utf8");
+	// vscode.workspace.fs.writeFile(vscode.Uri.file(file), buffer);
+}
 
 export async function getPackageJson(): Promise<string> {
 	const document = await vscode.workspace.openTextDocument(
 		`${vscode.workspace.rootPath}/package.json`
 	);
-	let text = document.getText();
-	return text;
+	return document.getText();
 }
 
-export function getArrayFromObject(object: any) {
+export function getArrayFromObject(object: any): Dependency[] {
 	return Object.keys(object).map((key) => new Dependency(key, object[key]));
 }
 
-export function getObjectFromArray(array: Dependency[]) {
-	let object: { [key: string]: string | undefined } = {};
+export function getObjectFromArray(array: Dependency[]): Dependencies {
+	const object: Dependencies = {};
 	array.forEach((element: Dependency) => {
 		object[element.getObject().name] = element.getObject().latestVersion;
 	});
