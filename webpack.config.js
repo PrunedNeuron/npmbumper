a4657d16d2c0ac6d6f690d64a088e2fa547e68f0
Restyled.io <commits@restyled.io>
2020-08-31 16:57:18 +0000

    Restyled by clang-format

--- webpack.config.js
+++ webpack.config.js
@@ -3,50 +3,32 @@ const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
 
 const config = {
-	target: "node",
-	entry: "./src/extension.ts",
-	output: {
-		path: path.resolve(__dirname, "dist"),
-		filename: "extension.js",
-		libraryTarget: "commonjs2",
-		devtoolModuleFilenameTemplate: "../[resource-path]"
-	},
-	externals: {
-		vscode: "commonjs vscode"
-	},
-	resolve: {
-		extensions: [".ts", ".js"]
-	},
-	module: {
-		rules: [
-			{
-				test: /\.ts$/,
-				exclude: /node_modules/,
-				use: [
-					{
-						loader: "ts-loader"
-					}
-				]
-			}
-		]
-	},
-	optimization: {
-		minimize: true,
-		minimizer: [
-			new TerserPlugin({
-				parallel: true,
-				terserOptions: {
-					compress: {
-						drop_console: true
-					},
-					output: {
-						comments: false
-					}
-				},
-				extractComments: false
-			})
-		]
-	}
+  target : "node",
+  entry : "./src/extension.ts",
+  output : {
+    path : path.resolve(__dirname, "dist"),
+    filename : "extension.js",
+    libraryTarget : "commonjs2",
+    devtoolModuleFilenameTemplate : "../[resource-path]"
+  },
+  externals : {vscode : "commonjs vscode"},
+  resolve : {extensions : [ ".ts", ".js" ]},
+  module : {
+    rules : [ {
+      test : /\.ts$/,
+      exclude : /node_modules/,
+      use : [ {loader : "ts-loader"} ]
+    } ]
+  },
+  optimization : {
+    minimize : true,
+    minimizer : [ new TerserPlugin({
+      parallel : true,
+      terserOptions :
+          {compress : {drop_console : true}, output : {comments : false}},
+      extractComments : false
+    }) ]
+  }
 };
 
 module.exports = config;
